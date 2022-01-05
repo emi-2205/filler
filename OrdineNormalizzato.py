be019909356aaaa87ef4ed12d6b553909038d50e
Paolo <S1092712@studenti.univpm.it>
2022-01-05 21:46:33 +0100

    OrdineNormalizzato FATTO

--- OrdineNormalizzato.py
+++ OrdineNormalizzato.py
@@ -13,14 +13,14 @@ id_fornitore_appoggio=[]
 id_prodotto_appoggio=[]
 
 mycursor= mydb.cursor()
-mycursor.execute('SELECT * FROM acciaio')
-acciaio = mycursor.fetchall()
+mycursor.execute('SELECT * FROM normalizzato')
+normalizzato = mycursor.fetchall()
 mycursor.execute('SELECT * FROM fornitore')
 fornitore = mycursor.fetchall()
 mycursor.execute('SELECT * FROM prodotto')
 prodotto = mycursor.fetchall()
 
-for a in acciaio:
+for a in normalizzato:
     id_materia_prima_appoggio.append(a[0])
 for f in fornitore:
     id_fornitore_appoggio.append(f[0])
@@ -31,7 +31,7 @@ for i in range(100):
     idMP.append(str(random.choice(id_materia_prima_appoggio)))
     idF.append(str(random.choice(id_fornitore_appoggio)))
     idP.append(str(random.choice(id_prodotto_appoggio)))
-    quantita.append(random.randrange(1, 5))
+    quantita.append(random.randrange(1, 100))
     date = datetime.date(random.randint(2019, 2021), random.randint(1, 12), random.randint(1, 28))
     dataOrdine.append(str(date))
     date = datetime.date(random.randint(2019, 2021), random.randint(1, 12), random.randint(1, 28))
@@ -48,7 +48,7 @@ mycursor= mydb.cursor()
 for i in range(0,100):
     val = (idMP[i], idF[i], idP[i], quantita[i], dataOrdine[i], dataArrivo[i])
 
-    sql = "INSERT INTO OrdineAcciaio (IDmateriaPrima, IDfornitore, IDprodotto, Quantita, DataOrdine, DataArrivo) " \
+    sql = "INSERT INTO OrdineNormalizzato (IDmateriaPrima, IDfornitore, IDprodotto, Quantita, DataOrdine, DataArrivo) " \
       "VALUES (%s, %s, %s, %s, %s, %s)"
 
     mycursor.execute(sql, val)
